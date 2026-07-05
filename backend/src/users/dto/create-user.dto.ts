f6a127835ed69d8aff0f07e2ecb6ea7322e71e22
shashi <shashikumar.katakam23@gmail.com>
2026-07-05 09:52:17 -0500

    Sprint 2: DTO validation completed

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @Length(2, 100)
+  name: string;
+
+  @IsString()
+  @Length(10, 15)
+  phone: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+}
